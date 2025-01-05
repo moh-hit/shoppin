9944c531071e2a1fe3c01b06f5d0815775fddfdd
moh-hit <moh.hit1012@gmail.com>
2025-01-05 23:21:53 +0530

    Enable Proguard for release builds, configure signing to optimize app size

--- src/components/Home/GeminiTab.tsx
+++ src/components/Home/GeminiTab.tsx
@@ -1,25 +1,34 @@
 import {View, Text, StyleSheet} from 'react-native';
 import React from 'react';
 import COLORS from '../../utils/colors';
+import {FONT_FAMILY} from '../../utils/consts';
 
-const GeminiTab = () => {
+const Gemini = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>GeminiTab</Text>
+      <Text style={styles.title}>Gemini</Text>
+      <Text style={styles.description}>
+        This feature is still lacking behind ChatGPT so shutting it down.
+      </Text>
     </View>
   );
 };
 
-export default GeminiTab;
+export default Gemini;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: COLORS.fill,
+    paddingHorizontal: 16,
+    gap: 12,
   },
-  text: {
-    color: 'red',
+  title: {
+    color: COLORS.text,
+    fontFamily: FONT_FAMILY.BOLD,
+    fontSize: 20,
+  },
+  description: {
+    color: COLORS.secondary,
+    fontFamily: FONT_FAMILY.REGULAR,
   },
 });
